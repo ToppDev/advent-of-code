04a6475eeda8b88225984146e0c9b522cca530fb
ToppDev <dev@topp.cc>
2024-01-24 23:31:03 +0100

    Criterion & Day-01 Part 1

--- 2023/rust/day-01/benches/benchmarks.rs
+++ 2023/rust/day-01/benches/benchmarks.rs
@@ -1,16 +1,17 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use day_01::*;
 
-fn main() {
-    // Run registered benchmarks.
-    divan::main();
+fn part1(c: &mut Criterion) {
+    c.bench_function("part1", |b| {
+        b.iter(|| part1::process(black_box(include_str!("../input1.txt"))))
+    });
 }
 
-#[divan::bench]
-fn part1() {
-    part1::process(divan::black_box(include_str!("../input1.txt",)));
+fn part2(c: &mut Criterion) {
+    c.bench_function("part2", |b| {
+        b.iter(|| part2::process(black_box(include_str!("../input2.txt"))))
+    });
 }
 
-#[divan::bench]
-fn part2() {
-    part2::process(divan::black_box(include_str!("../input2.txt",)));
-}
+criterion_group!(benches, part1, part2);
+criterion_main!(benches);
--- 2023/rust/day-01/src/part1.rs
+++ 2023/rust/day-01/src/part1.rs
@@ -1,5 +1,13 @@
+use itertools::Itertools;
+
 pub fn process(input: &str) -> String {
-    "".to_string()
+    input
+        .lines()
+        .map(|s| s.chars().filter(|c| c.is_digit(10)).collect())
+        .map(|s: String| format!("{}{}", s.chars().next().unwrap(), s.chars().last().unwrap()))
+        .map(|s| s.parse::<i32>().unwrap())
+        .sum::<i32>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -8,7 +16,10 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "";
-        assert_eq!("142", process(input));
+        let input = "1abc2
+pqr3stu8vwx
+a1b2c3d4e5f
+treb7uchet";
+        assert_eq!(process(input), "142");
     }
 }
