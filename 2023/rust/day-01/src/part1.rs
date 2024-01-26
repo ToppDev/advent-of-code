92d31862f01df19f2b85dcc0dad3de7958619e2f
Thomas Topp <thomas@topp.cc>
2024-01-26 11:18:47 +0100

    Day-01: Part 2

--- 2023/rust/day-01/src/part1.rs
+++ 2023/rust/day-01/src/part1.rs
@@ -1,16 +1,4 @@
-use itertools::Itertools;
-
 pub fn process(input: &str) -> String {
-    input
-        .lines()
-        .map(|s| s.chars().filter(|c| c.is_digit(10)).collect())
-        .map(|s: String| format!("{}{}", s.chars().next().unwrap(), s.chars().last().unwrap()))
-        .map(|s| s.parse::<i32>().unwrap())
-        .sum::<i32>()
-        .to_string()
-}
-
-pub fn process_iterator(input: &str) -> String {
     input
         .lines()
         .map(|line| {
@@ -19,8 +7,6 @@ pub fn process_iterator(input: &str) -> String {
             let last = it.last().unwrap_or(first);
             format!("{first}{last}").parse::<u32>().unwrap()
         })
-        // .map(|s| format!("{}{}", s.next().unwrap(), s.last()))
-        // .map(|s| s.parse::<i32>().unwrap())
         .sum::<u32>()
         .to_string()
 }
@@ -31,15 +17,6 @@ mod tests {
 
     #[test]
     fn test_process() {
-        let input = "1abc2
-pqr3stu8vwx
-a1b2c3d4e5f
-treb7uchet";
-        assert_eq!(process(input), "142");
-    }
-
-    #[test]
-    fn test_process_iterator() {
         let input = "1abc2
 pqr3stu8vwx
 a1b2c3d4e5f
--- 2023/rust/day-01/src/part2.rs
+++ 2023/rust/day-01/src/part2.rs
@@ -1,14 +1,70 @@
 pub fn process(input: &str) -> String {
-    "".to_string()
+    input.lines().map(process_line).sum::<u32>().to_string()
+}
+
+fn process_line(line: &str) -> u32 {
+    let mut idx = 0;
+    let line_iter = std::iter::from_fn(move || {
+        let line = &line[idx..];
+        let result = if line.starts_with("one") {
+            Some('1')
+        } else if line.starts_with("two") {
+            Some('2')
+        } else if line.starts_with("three") {
+            Some('3')
+        } else if line.starts_with("four") {
+            Some('4')
+        } else if line.starts_with("five") {
+            Some('5')
+        } else if line.starts_with("six") {
+            Some('6')
+        } else if line.starts_with("seven") {
+            Some('7')
+        } else if line.starts_with("eight") {
+            Some('8')
+        } else if line.starts_with("nine") {
+            Some('9')
+        } else {
+            line.chars().next()
+        };
+        idx += 1;
+        result
+    });
+    let mut it = line_iter.filter_map(|c| c.to_digit(10));
+    let first = it.next().expect("There should be a number");
+    let last = it.last().unwrap_or(first);
+    format!("{first}{last}").parse::<u32>().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
+    use rstest::rstest;
+
     use super::*;
 
     #[test]
     fn test_process() {
-        let input = "";
-        assert_eq!("142", process(input));
+        let input = "two1nine
+eightwothree
+abcone2threexyz
+xtwone3four
+4nineeightseven2
+zoneight234
+7pqrstsixteen";
+        assert_eq!(process(input), "281");
+    }
+
+    #[rstest]
+    #[case("two1nine", 29)]
+    #[case("eightwothree", 83)]
+    #[case("abcone2threexyz", 13)]
+    #[case("xtwone3four", 24)]
+    #[case("4nineeightseven2", 42)]
+    #[case("zoneight234", 14)]
+    #[case("7pqrstsixteen", 76)]
+    #[case("kvskplbpgfninesixvzkrv1fqnrjnrhvnpkpkhph27twonemv", 91)]
+    #[case("zkrv1fqnrjnrhvnpkpkhph27twonemv", 11)]
+    fn line_test(#[case] line: &str, #[case] expected: u32) {
+        assert_eq!(expected, process_line(line))
     }
 }
