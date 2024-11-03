90fe609c6864e9a05c2a7a5aa95808b0f1545c82
ToppDev <dev@topp.cc>
2024-11-04 00:09:14 +0100

    Day-08

--- 2023/rust/day-08/src/part1.rs
+++ 2023/rust/day-08/src/part1.rs
@@ -1,6 +1,28 @@
+use core::panic;
 use std::collections::HashMap;
 
+use nom::{
+    bytes::complete::tag,
+    character::complete::alpha1,
+    sequence::{separated_pair, terminated},
+    IResult,
+};
+
 use crate::custom_error::AocError;
+
+#[derive(Debug)]
+struct Node<'a> {
+    left: &'a str,
+    right: &'a str,
+}
+
+fn node(input: &str) -> IResult<&str, (&str, Node)> {
+    let (input, node) = terminated(alpha1, tag(" = ("))(input)?;
+    let (input, (left, right)) =
+        separated_pair(alpha1, tag(", "), alpha1)(input)?;
+
+    Ok((input, (node, Node { left, right })))
+}
 
 #[tracing::instrument]
 pub fn process(input: &str) -> miette::Result<String, AocError> {
@@ -8,18 +30,26 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
     let instructions = lines.next().unwrap();
     lines.next(); // empty line
 
-    let nodes: HashMap<_, _> = lines
-        .map(|line| {
-            dbg!(line);
-            (line, line)
+    let nodes: HashMap<_, _> =
+        lines.map(|line| node(line).unwrap().1).collect();
+
+    let mut current_node = "AAA";
+    let steps = instructions
+        .chars()
+        .cycle()
+        .take_while(|c| {
+            current_node = match c {
+                'L' => nodes[current_node].left,
+                'R' => nodes[current_node].right,
+                v => panic!("Can only be 'L' or 'R'. Got: {v}"),
+            };
+
+            current_node != "ZZZ"
         })
-        .collect();
+        .count()
+        + 1;
 
-    Ok("".to_string())
-}
-
-fn process_line(_line: &str) -> u32 {
-    todo!("day_08 - part 1 process_line")
+    Ok(steps.to_string())
 }
 
 #[cfg(test)]
--- 2023/rust/day-08/src/part2.rs
+++ 2023/rust/day-08/src/part2.rs
@@ -1,32 +1,109 @@
+use core::panic;
+use std::collections::HashMap;
+
+use nom::{
+    bytes::complete::tag,
+    character::complete::alphanumeric1,
+    sequence::{separated_pair, terminated},
+    IResult,
+};
+
 use crate::custom_error::AocError;
 
-#[tracing::instrument]
-pub fn process(_input: &str) -> miette::Result<String, AocError> {
-    todo!("day_08 - part 2")
+#[derive(Debug)]
+struct Node<'a> {
+    left: &'a str,
+    right: &'a str,
 }
 
-fn process_line(_line: &str) -> u32 {
-    todo!("day_08 - part 2 process_line")
+fn node(input: &str) -> IResult<&str, (&str, Node)> {
+    let (input, name) = terminated(alphanumeric1, tag(" = ("))(input)?;
+    let (input, (left, right)) =
+        separated_pair(alphanumeric1, tag(", "), alphanumeric1)(input)?;
+
+    Ok((input, (name, Node { left, right })))
+}
+
+#[tracing::instrument]
+pub fn process(input: &str) -> miette::Result<String, AocError> {
+    let mut lines = input.lines();
+    let instructions = lines.next().unwrap();
+    lines.next(); // empty line
+
+    let nodes: HashMap<_, _> =
+        lines.map(|line| node(line).unwrap().1).collect();
+
+    let mut starting_nodes: Vec<_> = nodes
+        .iter()
+        .filter(|x| x.0.ends_with('A'))
+        .map(|x| *x.0)
+        .collect();
+    println!("Starting nodes: {starting_nodes:?}");
+
+    // Searching till all starting nodes have a 'Z' at the end takes forever
+    // The input is hover arranged in a special way, so that it
+    // forms cycles (after some instructions it comes back to the start)
+    //
+    // With this the solution can be calculated by determining the cycle lengths
+    // and then calculating the least comman multiple (lcm) of the results.
+    let results = starting_nodes
+        .iter_mut()
+        .map(|current_node| {
+            instructions
+                .chars()
+                .cycle()
+                .take_while(|c| {
+                    *current_node = match c {
+                        'L' => nodes[current_node].left,
+                        'R' => nodes[current_node].right,
+                        v => panic!("Can only be 'L' or 'R'. Got: {v}"),
+                    };
+
+                    !current_node.ends_with('Z')
+                })
+                .count()
+                + 1
+        })
+        .collect::<Vec<usize>>();
+    println!("Cycles: {results:?}");
+
+    Ok(lcm(&results).to_string())
+}
+
+pub fn lcm(nums: &[usize]) -> usize {
+    if nums.len() == 1 {
+        return nums[0];
+    }
+    let a = nums[0];
+    let b = lcm(&nums[1..]);
+    a * b / gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: usize, b: usize) -> usize {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
 }
 
 #[cfg(test)]
 mod tests {
-    use rstest::rstest;
-
     use super::*;
 
     #[test]
     fn test_process() -> miette::Result<()> {
-        todo!("Test not yet written");
-        let input = "";
+        let input = "LR
 
-        assert_eq!(process(input)?, "");
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
+        assert_eq!(process(input)?, "6");
         Ok(())
-    }
-
-    #[rstest]
-    #[case("", 0)]
-    fn line_test(#[case] line: &str, #[case] expected: u32) {
-        assert_eq!(process_line(line), expected)
     }
 }
